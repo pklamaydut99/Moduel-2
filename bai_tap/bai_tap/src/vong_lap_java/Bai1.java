baac3d7e06cc891eadd55d04767e2d9020fd1bd1
khoinguyen <pklamaydut99@gmail.com>
2023-11-24 17:57:05 +0700

    up bt

---
+++ bai_tap/bai_tap/src/vong_lap_java/Bai1.java
@@ -0,0 +1,30 @@
+package vong_lap_java;
+
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        boolean flag = true;
+        do {
+            System.out.println("Chọn chức năng \n"+
+                    "1.In hình chữ nhật \n"+
+                    "2.In hình tam giác vuông \n"+
+                    "3.In hinh tam giác cân \n"+
+                    "4.Thoát");
+            int chon = Integer.parseInt(sc.nextLine());
+            switch (chon){
+                case 1:
+                    System.out.println("hiển thị");
+                case 1:
+                    System.out.println("hiển thị");
+                case 1:
+                    System.out.println("hiển thị");
+                case 1:
+                    System.out.println("hiển thị");
+                case 1:
+                    System.out.println("hiển thị");
+            }
+        }while (flag);
+    }
+}
